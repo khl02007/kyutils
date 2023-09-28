8a6a7c71e189ed9e795650f136487df25d7425d2
GitHub Action <action@github.com>
2023-09-28 00:24:54 +0000

    Format code with Black

--- src/kyutils/nwb/timestamps.py
+++ src/kyutils/nwb/timestamps.py
@@ -8,6 +8,7 @@ import numpy as np
 from hdmf.data_utils import GenericDataChunkIterator
 from spikeinterface.core import BaseRecording, BaseRecordingSegment
 
+
 class TimestampsExtractor(BaseRecording):
     def __init__(
         self,
