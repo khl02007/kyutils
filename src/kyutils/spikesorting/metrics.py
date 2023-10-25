727e80468cfe7037edf74d2daf1dc6e75715f467
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-10-25 10:20:30 -0700

    Remove met func

--- src/kyutils/spikesorting/metrics.py
+++ src/kyutils/spikesorting/metrics.py
@@ -15,6 +15,8 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
         },
     )
 
+    isi_violation_fraction_params = {"isi_threshold_ms": 2.0, "min_isi_ms": 0}
+
     nn_isolation_params = {
         "max_spikes": 1000,
         "min_spikes": 10,
@@ -36,8 +38,11 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
     nn_isolation = {}
     nn_noise = {}
     for i in waveform_extractor.sorting.get_unit_ids():
-        isi_violation_fraction[i] = compute_isi_violation_fractions(
-            waveform_extractor, this_unit_id=i
+        _, count = sq.compute_isi_violations(
+            waveform_extractor, unit_ids=[i], **isi_violation_fraction_params
+        )
+        isi_violation_fraction[i] = count / (
+            len(waveform_extractor.sorting.get_unit_spike_train(i)) - 1
         )
         nn_isolation[i] = sq.nearest_neighbors_isolation(
             waveform_extractor, this_unit_id=i, **nn_isolation_params
@@ -63,79 +68,3 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
         json.dump(metrics_dict, f)
 
     return metrics_dict
-
-
-def compute_isi_violation_fractions(
-    waveform_extractor: si.WaveformExtractor,
-    this_unit_id: str,
-    isi_threshold_ms: float = 2.0,
-    min_isi_ms: float = 0.0,
-):
-    """Computes the fraction of interspike interval violations.
-
-    Parameters
-    ----------
-    waveform_extractor: si.WaveformExtractor
-        The extractor object for the recording.
-
-    """
-
-    # Extract the total number of spikes that violated the isi_threshold for each unit
-    isi_violation_counts = np.asarray(
-        sq.compute_isi_violations(
-            waveform_extractor,
-            isi_threshold_ms=isi_threshold_ms,
-            min_isi_ms=min_isi_ms,
-        ).isi_violations_count
-    )
-
-    isi_violation_count = isi_violation_counts[
-        waveform_extractor.sorting.get_unit_ids() == this_unit_id
-    ]
-    total_spike_count = get_num_spikes(waveform_extractor, this_unit_id)
-    return isi_violation_count / (total_spike_count - 1)
-
-
-def get_peak_offset(
-    waveform_extractor: si.WaveformExtractor, peak_sign: str, **metric_params
-):
-    """Computes the shift of the waveform peak from center of window.
-
-    Parameters
-    ----------
-    waveform_extractor: si.WaveformExtractor
-        The extractor object for the recording.
-    peak_sign: str
-        The sign of the peak to compute. ('neg', 'pos', 'both')
-    """
-    if "peak_sign" in metric_params:
-        del metric_params["peak_sign"]
-    peak_offset_inds = si.get_template_extremum_channel_peak_shift(
-        waveform_extractor=waveform_extractor,
-        peak_sign=peak_sign,
-        **metric_params,
-    )
-    peak_offset = {key: int(abs(val)) for key, val in peak_offset_inds.items()}
-    return peak_offset
-
-
-def get_peak_channel(
-    waveform_extractor: si.WaveformExtractor, peak_sign: str, **metric_params
-):
-    """Computes the electrode_id of the channel with the extremum peak for each unit."""
-    if "peak_sign" in metric_params:
-        del metric_params["peak_sign"]
-    peak_channel_dict = si.get_template_extremum_channel(
-        waveform_extractor=waveform_extractor,
-        peak_sign=peak_sign,
-        **metric_params,
-    )
-    peak_channel = {key: int(val) for key, val in peak_channel_dict.items()}
-    return peak_channel
-
-
-def get_num_spikes(waveform_extractor: si.WaveformExtractor, this_unit_id: str):
-    """Computes the number of spikes for each unit."""
-    all_spikes = sq.compute_num_spikes(waveform_extractor)
-    unit_spikes = all_spikes[waveform_extractor.sorting.get_unit_ids() == this_unit_id]
-    return unit_spikes
