c0da707dc887d2402c0375584e98f084d74c4006
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-10-25 10:35:47 -0700

    Update metrics

--- src/kyutils/spikesorting/metrics.py
+++ src/kyutils/spikesorting/metrics.py
@@ -14,8 +14,18 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
             "seed": 0,
         },
     )
-
-    isi_violation_fraction_params = {"isi_threshold_ms": 2.0, "min_isi_ms": 0}
+    _, isi_violation_count = sq.compute_isi_violations(
+        waveform_extractor, isi_threshold_ms=2.0, min_isi_ms=0
+    )
+    num_spikes = sq.compute_num_spikes(waveform_extractor)
+    isi_violation_fraction = {
+        i: j / k
+        for i, j, k in zip(
+            isi_violation_count.keys(),
+            isi_violation_count.values(),
+            num_spikes.values(),
+        )
+    }
 
     nn_isolation_params = {
         "max_spikes": 1000,
@@ -25,6 +35,7 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
         "radius_um": 200,
         "seed": 0,
     }
+
     nn_noise_params = {
         "max_spikes": 1000,
         "min_spikes": 10,
@@ -34,17 +45,10 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
         "seed": 0,
     }
 
-    isi_violation_fraction = {}
     nn_isolation = {}
     nn_noise = {}
     for i in waveform_extractor.sorting.get_unit_ids():
-        _, count = sq.compute_isi_violations(
-            waveform_extractor, unit_ids=[i], **isi_violation_fraction_params
-        )
-        isi_violation_fraction[i] = count / (
-            len(waveform_extractor.sorting.get_unit_spike_train(i)) - 1
-        )
-        nn_isolation[i] = sq.nearest_neighbors_isolation(
+        nn_isolation[i], _ = sq.nearest_neighbors_isolation(
             waveform_extractor, this_unit_id=i, **nn_isolation_params
         )
         nn_noise[i] = sq.nearest_neighbors_noise_overlap(
@@ -54,6 +58,7 @@ def compute_standard_metrics(waveform_extractor, path_to_json):
     metrics_dict = {}
 
     metrics_dict["snr"] = snrs
+    metrics_dict["num_spikes"] = num_spikes
     metrics_dict["isi_violation_fraction"] = isi_violation_fraction
     metrics_dict["nn_isolation"] = nn_isolation
     metrics_dict["nn_noise_overlap"] = nn_noise
