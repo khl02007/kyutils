eed86afa15d00902976530ec8da76fbae455b853
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2024-06-11 15:33:14 -0700

    add new func waveform

--- src/kyutils/spikesorting/waveform.py
+++ src/kyutils/spikesorting/waveform.py
@@ -85,3 +85,50 @@ def plot_waveforms_singleshank(
     ax.set_axis_off()
 
     return ax
+
+
+def plot_waveforms_singleshank_new(
+    sorting_analyzer, unit_id, scale_x=150, scale_y=0.5, ax=None
+):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(3, 10))
+
+    waveforms = sorting_analyzer.get_extension("waveforms")
+    templates = sorting_analyzer.get_extension("templates")
+
+    nsamples = waveforms.nbefore + waveforms.nafter
+
+    t = np.linspace(0, 1, nsamples)
+
+    for i in range(len(sorting_analyzer.channel_ids)):
+        x, y = sorting_analyzer.get_channel_locations()[i]
+        ax.plot(
+            t + x / scale_x,
+            templates.get_unit_template(unit_id=unit_id)[:, i] + y / scale_y,
+            "k",
+        )
+
+    ref_loc = [
+        np.min(sorting_analyzer.get_channel_locations()[:, 0]),
+        np.min(sorting_analyzer.get_channel_locations()[:, 1]),
+    ]
+
+    ax.plot(
+        [
+            -3 / nsamples + ref_loc[0] / scale_x,
+            -3 / nsamples + ref_loc[0] / scale_x,
+        ],
+        [ref_loc[1] / scale_y, 100 + ref_loc[1] / scale_y],
+        "k-",
+    )
+    ax.text(
+        -4 / nsamples + ref_loc[0] / scale_x,
+        100 + ref_loc[1] / scale_y,
+        "100 $\mu$V",
+    )
+
+    ax.set_title(f"unit {unit_id}")
+
+    ax.set_axis_off()
+
+    return ax
